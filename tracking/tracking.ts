7287d087b8b07f099f2e54bdbd5da6408b7316d3
Thanh Phuong <buithanhphuong@amitgroup.vn>
2023-06-20 17:33:43 +0700

    fix: fix tracking

--- tracking/tracking.ts
+++ tracking/tracking.ts
@@ -45,7 +45,7 @@ declare function gtag(event: string, eventName: string, eventData: object): void
 export class TrackingUtils {
   static SendGA(action: string, label: string, category: string, eventName: string = "select_content") {
     try {
-      if (process.env.UNIVERSAL_ANALYTICS && process.env.UNIVERSAL_ANALYTICS === "true") {
+      if (process.env.UNIVERSAL_ANALYTICS && process.env.UNIVERSAL_ANALYTICS === "TRUE") {
         gtag("event", action, {
           event_category: category,
           event_label: label,
@@ -53,7 +53,7 @@ export class TrackingUtils {
         console.log("Universal Analytics: ", action, { category: category, label: label });
       }
 
-      if (process.env.GOOGLE_ANALYTICS_4 && process.env.GOOGLE_ANALYTICS_4 === "true") {
+      if (process.env.GOOGLE_ANALYTICS_4 && process.env.GOOGLE_ANALYTICS_4 === "TRUE") {
         gtag("event", eventName, {
           content_type: `${category}__${label}__${action}`,
         });
